3e4c684ddf6ff40e13102d1e2542133ddb0c469c
wawatt <1576055173@qq.com>
2025-09-28 20:03:06 +0800

    add triplets vcpkg.json

--- readme.md
+++ readme.md
@@ -1,9 +1,23 @@
 # 常用的vcpkg以外的库
 
-# 使用
+## 使用
+1. clone vcpkg
+```shell
+git clone https://github.com/microsoft/vcpkg.git\
+cd vcpkg
+.\bootstrap-vcpkg.bat
+git clone https://github.com/wawatt/my-vcpkg-overlay.git
 ```
-./vcpkg install ruckig --overlay-ports=E:\ENVs\vcpkgs\custom-overlay
+2. 全局安装
+复制my-vcpkg-overlay/vcpkg.json到vcpkg根目录
+```shell
+./vcpkg install
+./vcpkg.exe export --zip --output-dir=d:/vcpkg-export --x-all-installed
+```
+3. 单独安装
+```
+./vcpkg install ruckig --overlay-ports=E:\ENVs\vcpkgs\my-vcpkg-overlay
 ```
 
-# 参考
+## 参考
 * https://learn.microsoft.com/zh-cn/vcpkg/get_started/get-started-packaging?pivots=shell-cmd
---
+++ triplets/x64-windows.cmake
@@ -0,0 +1,35 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+# set(VCPKG_BUILD_TYPE release)
+set(MY_SHARED_LIST
+    "assimp" 
+    "casadi"
+    "hpp-fcl"
+    "fcl"
+    "onnxruntime-gpu"
+    "opencv4"
+    "ompl"
+    "openvino"
+    "python3"
+    "pinocchio"
+    "toppra"
+    "zstd"
+)
+list(FIND MY_SHARED_LIST "${PORT}" INDEX)
+
+# 如果INDEX大于或等于0，则字符串在列表中
+if(${INDEX} GREATER -1)
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+
+if("${PORT}" MATCHES "boost")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+
+if("${PORT}" MATCHES "qt5")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+
+message("")
+message("---------${PORT}--------LIBRARY: ${VCPKG_LIBRARY_LINKAGE}  CRT: ${VCPKG_CRT_LINKAGE}----")
