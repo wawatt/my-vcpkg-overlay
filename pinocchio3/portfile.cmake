87cccb3ea2f645e5383491a811707435c8731b27
WangDD <1576055173@qq.com>
2025-12-09 17:28:24 +0800

    update pino3

--- pinocchio3/portfile.cmake
+++ pinocchio3/portfile.cmake
@@ -38,15 +38,23 @@ vcpkg_cmake_configure(
         -DBUILD_TESTING=OFF 
         -DBUILD_WITH_COLLISION_SUPPORT=ON 
         -DBUILDING_ROS2_PACKAGE=OFF
+        -DCMAKE_RELEASE_POSTFIX=3
     OPTIONS_DEBUG   
-        -DCMAKE_DEBUG_POSTFIX=d
+        -DCMAKE_DEBUG_POSTFIX=3d
 )
 
 vcpkg_cmake_install()
 # vcpkg_fixup_pkgconfig()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/pinocchio)
+vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(COPY ${CURRENT_PACKAGES_DIR}/share/pinocchio/ DESTINATION ${CURRENT_PACKAGES_DIR}/share/pinocchio3)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/pinocchio")
+
+file(COPY ${CURRENT_PACKAGES_DIR}/include/pinocchio/ DESTINATION ${CURRENT_PACKAGES_DIR}/include/pinocchio3)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/pinocchio")
+
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
-# vcpkg_fixup_pkgconfig()
